174c9af70913a6b5019d28d9210e15870803dc4b
Prashant Kumar <programmerkrprashant@gmail.com>
2025-09-03 17:31:27 +0530

    Left Rotate an Array by k Positions

--- 06_DSA_With_JS_Mastering_Advance_Array/app.js
+++ 06_DSA_With_JS_Mastering_Advance_Array/app.js
@@ -1,4 +1,4 @@
-// Left rotation by 1 elelment
+//  Left Rotate an Array by 1 Positions
 
 // let arr = [1, 2, 3, 4, 5]
 // Ouput should be [2,3,4,5,1]
@@ -13,13 +13,44 @@
 // }
 // console.log(arr);
 
-// Right roatation by 1
+// Right Rotate an Array by 1 Positions
 // [1, 2, 3, 4, 5] [5, 1, 2, 3, 4]
+
+// let arr = [1, 2, 3, 4, 5]
+// let copy = arr[arr.length - 1]
+// for (let i = arr.length - 1; i > 0; i--) {
+//     // i=4, i=3, i=2, i=1
+//   arr[i] = arr[i - 1]
+//   // arr[4] = 4, arr[3] = 3, arr[2] = 2, arr[1] = 1,
+// }
+// arr[0] = copy;
+// // console.log(arr)
+
+/*
+
+📌 Question: Left Rotate an Array by k Positions
+Problem Statement:
+You are given an array arr of length n and an integer k. 
+Perform a left rotation on the array by k steps.
+
+In a single left rotation, 
+the first element of the array moves to the end of the array,
+ and all other elements shift one position to the left.
+
+Your task is to return the array after performing k left rotations.
+*/
+
 let arr = [1, 2, 3, 4, 5]
-let copy = arr[arr.length - 1] // 5
-let temp = 0 //
-for (let i = 0; i < arr.length - 1; i++) {
-    
- arr[i+1] = arr[i]
+// by 1 => [2, 3, 4, 5, 1] and by 2 => [3, 4, 5, 1, 2]
+let k = 2;
+k = k % arr.length
+let firstTwoElem = arr.slice(0, k)
+
+for (let i = 0; i < arr.length; i++) {
+  if (i < arr.length - k) {
+    arr[i] = arr[i + k]
+  } else {
+    arr[i] = firstTwoElem[i - (arr.length - k)]
+  }
 }
-console.log(arr)
+console.log(arr) // [ 3, 4, 5, 1, 2 ]
